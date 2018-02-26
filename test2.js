3b59d72f48afd6ec95387798d3d7e029683bd477
babacar thiam <thiamdu33@gmail.com>
2018-02-26 01:11:06 -0500

    Revert "Initial commit0"
    This reverts commit 417de7c16db0e1c71a05610263eb90d841817424.

--- test2.js
+++ test2.js
@@ -6,15 +6,27 @@ $(function(){
       optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
       acceptAllDevices:true
                       }
-                      navigator.bluetooth.requestDevice(options)
-                                .then(function(device){
-                                    console.log('> Found ' + device.name);
-
-
-                                })
-                                .catch(function(error){
-                                    console.log('Argh! ' + error);
+          navigator.bluetooth.requestDevice(options)
+                  .then(function(device){
+                                    device.gatt.connect().then(function(gattserveur){
+                                    gattserveur.getPrimaryService("c48e6067-5295-48d3-8d5c-0395f61792b1").then(function(gattservice){
+                                      gattservice.getCharacteristic("332f990e-8f0c-4ab8-8713-f51d63dd5910").then(function(gattcharacteristic)
+                                        {
+                                          gattcharacteristic.startNotifications().then(function(gattcharacteristic){
+                                            gattcharacteristic.addEventListener("characteristicvaluechanged", function(event){
+                                              var value= event.target.value.getUint8(0);
+                                              $("notifiedValue").text("", +value);
+                                              .catch(function(error){
+                                                  console.log('Argh! ' + error);
+                                              });
+                                         });
                                 });
+                              });
+                            });
+                          });
+                        });
+
+
         //var dev=JSON.stringify(options, null, 2)
       //  alert('Name: ' + dev.filters);
         // Do something with the device.
