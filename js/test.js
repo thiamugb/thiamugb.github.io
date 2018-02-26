d3128a2dd38aebfb0ee7bb904177ef41b75c61fa
babacar thiam <thiamdu33@gmail.com>
2018-02-25 23:45:22 -0500

    Initial commit1

--- js/test.js
+++ js/test.js
@@ -1,20 +1,33 @@
-$(function(){
-    $("#test").click(function(){
-        //var dev=JSON.stringify(options, null, 2)
-      //  alert('Name: ' + dev.filters);
-        // Do something with the device.
+
+    function connect() {
+        console.log('Requesting Bluetooth Device...');
         let options = {
 
     optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
     acceptAllDevices:true
   }
 
-  navigator.bluetooth.requestDevice(options).then(function(device) {
-    console.log('Name: ' + device.name);
-    // Do something with the device.
-  })
-  .catch(function(error) {
-    console.log("Something went wrong. " + error);
-  });
-  });
-    });
+  navigator.bluetooth.requestDevice(options)
+            .then(device => {
+                console.log('> Found ' + device.name);
+                console.log('Connecting to GATT Server...');
+                device.addEventListener('gattserverdisconnected', onDisconnected)
+                return device.gatt.connect();
+            })
+            .then(server => {
+                console.log('Getting Service 0xffe5 - Light control...');
+                return server.getPrimaryService(0xffe5);
+            })
+            .then(service => {
+                console.log('Getting Characteristic 0xffe9 - Light control...');
+                return service.getCharacteristic(0xffe9);
+            })
+            .then(characteristic => {
+                console.log('All ready!');
+                ledCharacteristic = characteristic;
+                onConnected();
+            })
+            .catch(error => {
+                console.log('Argh! ' + error);
+            });
+    }
