a318ca988e6cd9bf443bea7c6e89b4fb0e2b0341
babacar thiam <thiamdu33@gmail.com>
2018-02-26 01:43:45 -0500

    Initial commit111

--- test1.js
+++ test1.js
@@ -14,22 +14,10 @@
                 console.log('Connecting to GATT Server...');
                 //device.addEventListener('gattserverdisconnected', onDisconnected)
                 alert("Ok done")
-                device.gatt.connect().then(gattserveur =>{
-                gattserveur.getPrimaryService("c48e6067-5295-48d3-8d5c-0395f61792b1").then(gattservice =>{
-                  gattservice.getCharacteristic("332f990e-8f0c-4ab8-8713-f51d63dd5910").then(gattcharacteristic =>
-                    {
-                      gattcharacteristic.startNotifications().then(gattcharacteristic =>{
-                        gattcharacteristic.addEventListener("characteristicvaluechanged", event =>{
-                          var value= event.target.value.getUint8(0);
-                          $("notifiedValue").text("", +value);
+              })
                           .catch(error => {
                 console.log('Argh! ' + error);
             });
-          });
-        });
-      });
-    });
-  });
-});
+
 
     }
