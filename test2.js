b00e4c91d920ad8af0982afcf1da9435bed5205b
babacar thiam <thiamdu33@gmail.com>
2018-02-26 00:35:09 -0500

    Initial commit2

--- test2.js
+++ test2.js
@@ -1,13 +1,20 @@
 $(function(){
-  var options = {
-
-optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
-acceptAllDevices:true
-                }
     $("#test").click(function() {
+
+        let options = {
+
+      optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
+      acceptAllDevices:true
+                      }
+                      navigator.bluetooth.requestDevice(options)
+                                .then(device => {
+                                    console.log('> Found ' + device.name);
+                                })
+                                .catch(error => {
+                                    console.log('Argh! ' + error);
+                                });
         //var dev=JSON.stringify(options, null, 2)
       //  alert('Name: ' + dev.filters);
         // Do something with the device.
-        alert("Bonjour thiam");
     });
 });
