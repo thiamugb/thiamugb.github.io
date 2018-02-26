557114d4ba4a9c4576056b5e408c0c10cb353ea2
babacar thiam <thiamdu33@gmail.com>
2018-02-26 01:47:48 -0500

    Initial commit1111

--- js/test.js
+++ js/test.js
@@ -11,7 +11,6 @@
                 console.log('> Found ' + device.name);
                 console.log('Connecting to GATT Server...');
                 //device.addEventListener('gattserverdisconnected', onDisconnected)
-                alert("Ok done")
                 device.gatt.connect().then(gattserveur =>{
                 gattserveur.getPrimaryService("c48e6067-5295-48d3-8d5c-0395f61792b1").then(gattservice =>{
                   gattservice.getCharacteristic("332f990e-8f0c-4ab8-8713-f51d63dd5910").then(gattcharacteristic =>
