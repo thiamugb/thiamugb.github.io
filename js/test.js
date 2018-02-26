1c6192b47728d52c6737ab564242525f5331dba1
babacar thiam <thiamdu33@gmail.com>
2018-02-26 00:45:33 -0500

    Initial commit0

--- js/test.js
+++ js/test.js
@@ -11,23 +11,24 @@
             .then(device => {
                 console.log('> Found ' + device.name);
                 console.log('Connecting to GATT Server...');
-                device.addEventListener('gattserverdisconnected', onDisconnected)
-                return device.gatt.connect();
-            })
-            .then(server => {
-                console.log('Getting Service 0xffe5 - Light control...');
-                return server.getPrimaryService(0xffe5);
-            })
-            .then(service => {
-                console.log('Getting Characteristic 0xffe9 - Light control...');
-                return service.getCharacteristic(0xffe9);
-            })
-            .then(characteristic => {
-                console.log('All ready!');
-                ledCharacteristic = characteristic;
-                onConnected();
-            })
-            .catch(error => {
+                //device.addEventListener('gattserverdisconnected', onDisconnected)
+
+                device.gatt.connect().then(gattserveur =>{
+                gattserveur.getPrimaryService("c48e6067-5295-48d3-8d5c-0395f61792b1").then(gattservice =>{
+                  gattservice.getCharacteristic("332f990e-8f0c-4ab8-8713-f51d63dd5910").then(gattcharacteristic =>
+                    {
+                      gattcharacteristic.startNotifications().then(gattcharacteristic =>{
+                        gattcharacteristic.addEventListener("characteristicvaluechanged", event =>{
+                          var value= event.target.value.getUint8(0);
+                          $("notifiedValue").text("", +value);
+                          .catch(error => {
                 console.log('Argh! ' + error);
             });
+          });
+        });
+      });
+    });
+  });
+});
+
     }
