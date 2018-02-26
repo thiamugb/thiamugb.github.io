665114d7037bf7ddda7239142fbfdc6c5402142f
babacar thiam <thiamdu33@gmail.com>
2018-02-26 00:25:59 -0500

    Initial commit2

---
+++ test2.js
@@ -0,0 +1,13 @@
+$(function(){
+  var options = {
+
+optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
+acceptAllDevices:true
+                }
+    $("#test").click(() => {
+        //var dev=JSON.stringify(options, null, 2)
+      //  alert('Name: ' + dev.filters);
+        // Do something with the device.
+        alert("Bonjour thiam");
+    });
+});
