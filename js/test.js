419cd46c88879d04c2ac9874674137a419a9d7d0
babacar thiam <thiamdu33@gmail.com>
2018-02-26 00:07:23 -0500

    Initial commit2

--- js/test.js
+++ js/test.js
@@ -10,8 +10,23 @@
   navigator.bluetooth.requestDevice(options)
             .then(device => {
                 console.log('> Found ' + device.name);
+                console.log('Connecting to GATT Server...');
+                device.addEventListener('gattserverdisconnected', onDisconnected)
+                return device.gatt.connect();
             })
-
+            .then(server => {
+                console.log('Getting Service 0xffe5 - Light control...');
+                return server.getPrimaryService(0xffe5);
+            })
+            .then(service => {
+                console.log('Getting Characteristic 0xffe9 - Light control...');
+                return service.getCharacteristic(0xffe9);
+            })
+            .then(characteristic => {
+                console.log('All ready!');
+                ledCharacteristic = characteristic;
+                onConnected();
+            })
             .catch(error => {
                 console.log('Argh! ' + error);
             });
