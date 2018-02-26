424775e49e9943a9ac71140d1e3b3a0e3f7b36ee
babacar thiam <thiamdu33@gmail.com>
2018-02-26 01:50:14 -0500

    Initial commit11111

--- js/test1.js
+++ js/test1.js
@@ -1,20 +1,20 @@
-$(function(){
-    $("#test").click(function(){
-        //var dev=JSON.stringify(options, null, 2)
-      //  alert('Name: ' + dev.filters);
-        // Do something with the device.
-        let options = {
+function connect() {
+    console.log('Requesting Bluetooth Device...');
+    let options = {
 
-    optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
-    acceptAllDevices:true
-  }
+optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
+acceptAllDevices:true
+}
+navigator.bluetooth.requestDevice(options)
+        .then(device => {
+            console.log('> Found ' + device.name);
+            console.log('Connecting to GATT Server...');
+            //device.addEventListener('gattserverdisconnected', onDisconnected)
+            alert("Ok done")
+          })
+                      .catch(error => {
+            console.log('Argh! ' + error);
+        });
 
-  navigator.bluetooth.requestDevice(options).then(function(device) {
-    console.log('Name: ' + device.name);
-    // Do something with the device.
-  })
-  .catch(function(error) {
-    console.log("Something went wrong. " + error);
-  });
-  });
-    });
+
+}
--- test1.js
+++ test1.js
@@ -5,14 +5,11 @@
     optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
     acceptAllDevices:true
   }
-  alert("On demmare")
-
   navigator.bluetooth.requestDevice(options)
             .then(device => {
                 console.log('> Found ' + device.name);
                 console.log('Connecting to GATT Server...');
                 //device.addEventListener('gattserverdisconnected', onDisconnected)
-                alert("Ok done")
               })
                           .catch(error => {
                 console.log('Argh! ' + error);
