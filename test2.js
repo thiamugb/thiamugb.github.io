05caa63afa44f00200ff4f44a4507be6fa5aa772
babacar thiam <thiamdu33@gmail.com>
2018-02-26 00:30:14 -0500

    Initial commit2

--- test2.js
+++ test2.js
@@ -4,7 +4,7 @@ $(function(){
 optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
 acceptAllDevices:true
                 }
-    $("#test").click(() => {
+    $("#test").click(function() {
         //var dev=JSON.stringify(options, null, 2)
       //  alert('Name: ' + dev.filters);
         // Do something with the device.
