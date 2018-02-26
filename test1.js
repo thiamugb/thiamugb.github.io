9c59de82fb838d5d2c1278152a7ac380974f19fc
babacar thiam <thiamdu33@gmail.com>
2018-02-26 00:20:55 -0500

    Initial commit2

--- test1.js
+++ test1.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
-    $("#test").click(function(){
+  var options = {
+
+optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
+acceptAllDevices:true
+                }
+    $("#test").click(() => {
         //var dev=JSON.stringify(options, null, 2)
       //  alert('Name: ' + dev.filters);
         // Do something with the device.
         alert("Bonjour thiam");
-        let options = {
-
-    optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
-    acceptAllDevices:true
-  }
-
-  navigator.bluetooth.requestDevice(options).then(function(device) {
-    console.log('Name: ' + device.name);
-    // Do something with the device.
-  })
-  .catch(function(error) {
-    console.log("Something went wrong. " + error);
-  });
-  });
+  navigator.bluetooth.requestDevice(options).then(device => {
+    device.gatt.connect().then(gattserveur =>{
+    gattserveur.getPrimaryService("c48e6067-5295-48d3-8d5c-0395f61792b1").then(gattservice =>{
+      gattservice.getCharacteristic("332f990e-8f0c-4ab8-8713-f51d63dd5910").then(gattcharacteristic =>
+        {
+          gattcharacteristic.startNotifications().then(gattcharacteristic =>{
+            gattcharacteristic.addEventListener("characteristicvaluechanged", event =>{
+              var value= event.target.value.getUint8(0);
+              $("notifiedValue").text("", +value);
+         });
     });
+    });
+    });
+    });
+    });
+  });
+  });
+  
