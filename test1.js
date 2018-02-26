fbe1930eb06f68d6f140ad17c8f841c7e058047e
babacar thiam <thiamdu33@gmail.com>
2018-02-25 23:54:57 -0500

    Initial commit2

--- test1.js
+++ test1.js
@@ -1,8 +1,9 @@
-$(function(){
+$(document).ready(function(){
     $("#test").click(function(){
         //var dev=JSON.stringify(options, null, 2)
       //  alert('Name: ' + dev.filters);
         // Do something with the device.
+        alert("Bonjour thiam");
         let options = {
 
     optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
