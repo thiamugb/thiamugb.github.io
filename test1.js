bc8ec8acfee03337faa3ee7727c4dbb1082f016c
babacar thiam <thiamdu33@gmail.com>
2018-02-26 21:16:54 -0500

    Initial

--- test1.js
+++ test1.js
@@ -5,7 +5,6 @@
     optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
     acceptAllDevices:true
   }
-  $("#notifiedValue").text("bonjour thiam");
   navigator.bluetooth.requestDevice(options)
             .then(device => {
                 console.log('> Found ' + device.name);
@@ -13,7 +12,7 @@
                 //device.addEventListener('gattserverdisconnected', onDisconnected)
                 device.gatt.connect().then(gattserveur =>{
                 gattserveur.getPrimaryService("c48e6067-5295-48d3-8d5c-0395f61792b1").then(gattservice =>{
-                  gattservice.getCharacteristic("332f990e-8f0c-4ab8-8713-f51d63dd5910").then(gattcharacteristic =>
+                  gattservice.getCharacteristic("0d9dd6ab-e0e4-4d2e-acc1-8f0e60916cd4").then(gattcharacteristic =>
                     {
                       gattcharacteristic.startNotifications().then(gattcharacteristic =>{
                         gattcharacteristic.addEventListener("characteristicvaluechanged", event =>{
