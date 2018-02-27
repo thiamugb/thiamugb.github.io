d123a57e480ad6b6bf4c7bcf2b9748ab9a2c7119
babacar thiam <thiamdu33@gmail.com>
2018-02-26 20:52:22 -0500

    I

--- test1.js
+++ test1.js
@@ -5,6 +5,7 @@
     optionalServices: ['c48e6067-5295-48d3-8d5c-0395f61792b1'],
     acceptAllDevices:true
   }
+  $("#notifiedValue").text("bonjour thiam");
   navigator.bluetooth.requestDevice(options)
             .then(device => {
                 console.log('> Found ' + device.name);
